884417fc2912697c599101a2163b7ba852c2c2e0
Zhijian Yan <937279457@qq.com>
2026-05-17 03:03:30 +0800

    refactor: redesign tkey core architecture

--- tickey.c
+++ tickey.c
@@ -4,120 +4,64 @@
 #include "tickey.h"
 #include <string.h>
 
-#define TKEY_STATE_UNPRESSED 0
-#define TKEY_STATE_PRESSED 1
+typedef enum {
+    TKEY_STATE_UNPRESSED = 0,
+    TKEY_STATE_PRESSED,
+} tkey_state_t;
 
-static struct tkey {
-    tkey_event_cb_t event_cb;
-    tkey_detect_cb_t detect_cb;
-    void *user_data;
-    uint16_t hold_ticks;
-    uint16_t debounce_ticks;
-    uint16_t multi_press_interval_ticks;
-    uint16_t pressed_ticks;
-    uint16_t multi_press_ticks;
-    uint8_t multi_press_count;
-    uint8_t press_state : 1;
-    uint8_t flag_long_pressed : 1;
-    uint8_t enabled : 1;
-    tkey_handle_t prev;
-    tkey_handle_t next;
-} *head;
+typedef struct {
+    tkey_t *key;
+    tkey_event_t event;
+    uint8_t press_count;
+} tkey_message_t;
 
-volatile static uint32_t flag_critical;
+typedef struct {
+    volatile uint8_t write_index;
+    volatile uint8_t read_index;
+    tkey_message_t buffer[TKEY_QUEUE_SIZE];
+} tkey_queue_t;
 
-tkey_handle_t tkey_create(tkey_config_t *config) {
-    tkey_handle_t key;
-    if (!config)
-        return NULL;
-    key = (tkey_handle_t)tkey_malloc(sizeof(struct tkey));
-    if (!key)
-        return NULL;
-    memset(key, 0, sizeof(struct tkey));
-    key->event_cb = config->event_cb;
-    key->detect_cb = config->detect_cb;
-    key->user_data = config->user_data;
-    key->hold_ticks = config->hold_ticks;
-    key->debounce_ticks = config->debounce_ticks;
-    key->multi_press_interval_ticks = config->multi_press_interval_ticks;
-    key->pressed_ticks = 0;
-    key->multi_press_ticks = 0;
-    key->multi_press_count = 0;
+static tkey_queue_t tkey_queue;
+
+static int tkey_queue_send(tkey_queue_t *queue, const tkey_message_t *message) {
+    uint8_t w;
+    uint8_t next;
+    w = queue->write_index;
+    next = (w + 1) & (TKEY_QUEUE_SIZE - 1);
+    if (next == queue->read_index)
+        return -TKEY_EAGAIN;
+    queue->buffer[w] = *message;
+    queue->write_index = next;
+    return 0;
+}
+
+static int tkey_queue_receive(tkey_queue_t *queue, tkey_message_t *message) {
+    uint8_t r;
+    r = queue->read_index;
+    if (r == queue->write_index)
+        return -TKEY_EAGAIN;
+    *message = queue->buffer[r];
+    queue->read_index = (r + 1) & (TKEY_QUEUE_SIZE - 1);
+    return 0;
+}
+
+int tkey_init(tkey_t *key, tkey_event_cb_t event_cb, tkey_detect_cb_t detect_cb,
+              void *user_data) {
+    if (!key || !event_cb || !detect_cb)
+        return -TKEY_EINVAL;
+    memset(key, 0, sizeof(tkey_t));
+    key->detect_cb = detect_cb;
+    key->event_cb = event_cb;
+    key->user_data = user_data;
+    key->debounce_ticks = TKEY_DEFAULT_DEBOUNCE;
+    key->hold_ticks = TKEY_DEFAULT_HOLD;
+    key->multi_press_interval_ticks = TKEY_DEFAULT_MULTI_PRESS_INTERVAL;
     key->press_state = TKEY_STATE_UNPRESSED;
-    key->flag_long_pressed = 0;
-    key->enabled = 0;
-    return key;
+    return 0;
 }
 
-void tkey_delete(tkey_handle_t key) {
-    if (!key || key->enabled)
-        return;
-    tkey_free(key);
-}
-
-tkey_handle_t tkey_create_default(tkey_event_cb_t event_cb,
-                                  tkey_detect_cb_t detect_cb, void *user_data) {
-    tkey_config_t config;
-    config.debounce_ticks = TKEY_DEFAULT_DEBOUNCE;
-    config.detect_cb = detect_cb;
-    config.event_cb = event_cb;
-    config.hold_ticks = TKEY_DEFAULT_HOLD;
-    config.multi_press_interval_ticks = TKEY_DEFAULT_MULTI_PRESS_INTERVAL;
-    config.user_data = user_data;
-    return tkey_create(&config);
-}
-
-static void tkey_list_add(tkey_handle_t key) {
-    ++flag_critical;
-    if (head == NULL) {
-        head = key;
-        head->prev = key;
-    } else {
-        key->prev = head->prev;
-        head->prev->next = key;
-        head->prev = key;
-    }
-    key->next = NULL;
-    --flag_critical;
-}
-
-static void tkey_list_del(tkey_handle_t key) {
-    ++flag_critical;
-    if (key == head) {
-        if (key->next) {
-            key->next->prev = head->prev;
-            head = key->next;
-        } else
-            head = NULL;
-    } else {
-        key->prev->next = key->next;
-        if (key->next)
-            key->next->prev = key->prev;
-        else
-            head->prev = key->prev;
-    }
-    key->prev = NULL;
-    key->next = NULL;
-    --flag_critical;
-}
-
-void tkey_enable(tkey_handle_t key) {
-    if (!key || key->enabled)
-        return;
-    key->enabled = 1;
-    if (!key->detect_cb || !key->event_cb)
-        return;
-    tkey_list_add(key);
-}
-
-void tkey_disable(tkey_handle_t key) {
-    if (!key || !key->enabled)
-        return;
-    tkey_list_del(key);
-    key->enabled = 0;
-}
-
-static void tkey_key_handler(tkey_handle_t key) {
+static tkey_event_t tkey_handler(tkey_t *key, int detect_value,
+                                 uint8_t *press_count) {
     tkey_event_t event = TKEY_EVENT_NULL;
     if (key->multi_press_count) {
         if (key->multi_press_ticks < TKEY_MAX_TICKS)
@@ -131,7 +75,7 @@ static void tkey_key_handler(tkey_handle_t key) {
         }
     }
     if (key->press_state == TKEY_STATE_UNPRESSED) {
-        if (key->detect_cb(key->user_data)) {
+        if (detect_value) {
             if (key->pressed_ticks >= key->debounce_ticks) {
                 key->press_state = TKEY_STATE_PRESSED;
                 key->pressed_ticks = 0;
@@ -148,11 +92,11 @@ static void tkey_key_handler(tkey_handle_t key) {
     } else if (key->press_state == TKEY_STATE_PRESSED) {
         if (key->pressed_ticks < TKEY_MAX_TICKS)
             ++key->pressed_ticks;
-        if (!key->detect_cb(key->user_data)) {
+        if (!detect_value) {
             key->press_state = TKEY_STATE_UNPRESSED;
             if (key->flag_long_pressed) {
                 key->flag_long_pressed = 0;
-                event = TKEY_EVENT_LONG_RELEASE;
+                event |= TKEY_EVENT_LONG_RELEASE;
             } else if (key->multi_press_count > 1)
                 event |= TKEY_EVENT_MULTI_RELEASE;
             else
@@ -163,61 +107,70 @@ static void tkey_key_handler(tkey_handle_t key) {
             event |= TKEY_EVENT_LONG_PRESS;
         }
     }
-    if (event) {
-        key->event_cb(key, event, key->multi_press_count, key->user_data);
-        if (event & (TKEY_EVENT_PRESS_TIMEOUT | TKEY_EVENT_RELEASE_TIMEOUT))
-            key->multi_press_count = 0;
+    *press_count = key->multi_press_count;
+    if (event & (TKEY_EVENT_PRESS_TIMEOUT | TKEY_EVENT_RELEASE_TIMEOUT)) {
+        key->multi_press_count = 0;
     }
+    return event;
 }
 
-void tkey_handler(void) {
-    tkey_handle_t temp = head;
-    tkey_handle_t next;
-    if (flag_critical)
-        return;
-    while (temp) {
-        next = temp->next;
-        tkey_key_handler(temp);
-        temp = next;
+int tkey_scan(tkey_t key_arr[], uint32_t num) {
+    int tkey_lock_state;
+    int detect_value;
+    int ret = 0;
+    tkey_message_t message;
+    if (!key_arr)
+        return -TKEY_EINVAL;
+    while (num--) {
+        message.key = &key_arr[num];
+        detect_value = message.key->detect_cb(message.key->user_data);
+        tkey_lock_state = tkey_lock();
+        message.event =
+            tkey_handler(message.key, detect_value, &message.press_count);
+        tkey_unlock(tkey_lock_state);
+        if (message.event) {
+            ret = tkey_queue_send(&tkey_queue, &message);
+            if (ret)
+                continue;
+        }
     }
+    return ret;
 }
 
-void tkey_register_callback(tkey_handle_t key, tkey_event_cb_t event_cb,
-                            tkey_detect_cb_t detect_cb, void *user_data) {
-    if (!key || key->enabled)
-        return;
-    key->user_data = user_data;
-    key->event_cb = event_cb;
-    key->detect_cb = detect_cb;
+void tkey_dispatch(uint8_t max_event_num) {
+    tkey_message_t message;
+    while (max_event_num-- && !tkey_queue_receive(&tkey_queue, &message))
+        message.key->event_cb(message.key, message.event, message.press_count,
+                              message.key->user_data);
 }
 
-void tkey_set_hold(tkey_handle_t key, uint16_t hold_ticks) {
+int tkey_set_hold(tkey_t *key, uint16_t hold_ticks) {
+    int tkey_lock_state;
     if (!key)
-        return;
+        return -TKEY_EINVAL;
+    tkey_lock_state = tkey_lock();
     key->hold_ticks = hold_ticks;
+    tkey_unlock(tkey_lock_state);
+    return 0;
 }
 
-void tkey_set_debounce(tkey_handle_t key, uint16_t debounce_ticks) {
+int tkey_set_debounce(tkey_t *key, uint16_t debounce_ticks) {
+    int tkey_lock_state;
     if (!key)
-        return;
+        return -TKEY_EINVAL;
+    tkey_lock_state = tkey_lock();
     key->debounce_ticks = debounce_ticks;
+    tkey_unlock(tkey_lock_state);
+    return 0;
 }
 
-void tkey_set_multi_press_interval(tkey_handle_t key,
-                                   uint16_t multi_press_interval_ticks) {
+int tkey_set_multi_press_interval(tkey_t *key,
+                                  uint16_t multi_press_interval_ticks) {
+    int tkey_lock_state;
     if (!key)
-        return;
+        return -TKEY_EINVAL;
+    tkey_lock_state = tkey_lock();
     key->multi_press_interval_ticks = multi_press_interval_ticks;
-}
-
-uint16_t tkey_get_pressed_ticks(tkey_handle_t key) {
-    if (!key)
-        return 0;
-    return key->pressed_ticks;
-}
-
-uint16_t tkey_get_multi_press_ticks(tkey_handle_t key) {
-    if (!key)
-        return 0;
-    return key->multi_press_ticks;
+    tkey_unlock(tkey_lock_state);
+    return 0;
 }
--- tickey.h
+++ tickey.h
@@ -9,32 +9,44 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-#include <stdlib.h>
 
-#define tkey_malloc(size) malloc(size)
-#define tkey_free(ptr) free(ptr)
+static inline int tkey_lock(void) {
+    /* Disable interrupts if needed */
+    return 0;
+}
+
+static inline void tkey_unlock(int tkey_lock_state) {
+    /* Restore interrupt state */
+    (void)tkey_lock_state;
+}
+
 #define TKEY_DEFAULT_DEBOUNCE (1)
 #define TKEY_DEFAULT_HOLD (50)
 #define TKEY_DEFAULT_MULTI_PRESS_INTERVAL (30)
+#define TKEY_QUEUE_SIZE (16)
+#if (TKEY_QUEUE_SIZE > 256)
+#error "TKEY_QUEUE_SIZE must be <= 256"
+#endif
+#if (TKEY_QUEUE_SIZE & (TKEY_QUEUE_SIZE - 1)) != 0
+#error "TKEY_QUEUE_SIZE must be power of 2"
+#endif
 #define TKEY_MAX_TICKS (0xFFFF)
 #define TKEY_MAX_COUNT (0xFF)
+#define TKEY_EINVAL 22
+#define TKEY_EAGAIN 11
 
-/**
- * @brief Enumeration of key events
- */
+typedef struct tkey tkey_t;
+
 typedef enum {
-    TKEY_EVENT_NULL = 0x00,         /**< Key null event */
-    TKEY_EVENT_PRESS = 0x01,        /**< Key press event */
-    TKEY_EVENT_RELEASE = 0x02,      /**< Key release event after single press */
-    TKEY_EVENT_LONG_PRESS = 0x04,   /**< Long press event */
-    TKEY_EVENT_LONG_RELEASE = 0x08, /**< Key release event after long press */
-    TKEY_EVENT_MULTI_PRESS = 0x10,  /**< Multiple press event */
-    TKEY_EVENT_MULTI_RELEASE =
-        0x20, /**< Key release event after multiple presses */
-    TKEY_EVENT_PRESS_TIMEOUT =
-        0x40, /**< Multi-press detection timeout in pressed state */
-    TKEY_EVENT_RELEASE_TIMEOUT =
-        0x80, /**< Multi-press detection timeout in released state */
+    TKEY_EVENT_NULL = 0,
+    TKEY_EVENT_PRESS = (1 << 0),
+    TKEY_EVENT_RELEASE = (1 << 1),
+    TKEY_EVENT_LONG_PRESS = (1 << 2),
+    TKEY_EVENT_LONG_RELEASE = (1 << 3),
+    TKEY_EVENT_MULTI_PRESS = (1 << 4),
+    TKEY_EVENT_MULTI_RELEASE = (1 << 5),
+    TKEY_EVENT_PRESS_TIMEOUT = (1 << 6),
+    TKEY_EVENT_RELEASE_TIMEOUT = (1 << 7),
     TKEY_EVENT_ALL_PRESS =
         (TKEY_EVENT_PRESS | TKEY_EVENT_LONG_PRESS | TKEY_EVENT_MULTI_PRESS),
     TKEY_EVENT_ALL_RELEASE = (TKEY_EVENT_RELEASE | TKEY_EVENT_LONG_RELEASE |
@@ -43,157 +55,33 @@ typedef enum {
     TKEY_EVENT_DEFAULT_RELEASE = TKEY_EVENT_ALL_RELEASE,
 } tkey_event_t;
 
-/**
- * @brief Opaque handle type for key instances
- */
-typedef struct tkey *tkey_handle_t;
-
-/**
- * @brief Key event callback function type
- *
- * @param key Handle of the key that triggered the event
- * @param event Type of key event that occurred
- * @param multi_press_count Multi-press count (2 for double, 3 for triple, etc.)
- * @param user_data User-defined data passed to both event and detection
- * callbacks
- */
-typedef void (*tkey_event_cb_t)(tkey_handle_t key, tkey_event_t event,
+typedef void (*tkey_event_cb_t)(tkey_t *key, tkey_event_t event,
                                 uint8_t multi_press_count, void *user_data);
 
-/**
- * @brief Key detection callback function type
- *
- * @param user_data User-defined data passed to both event and detection
- * callbacks
- * @return
- * Pressed level if key is pressed, otherwise released level
- */
 typedef int (*tkey_detect_cb_t)(void *user_data);
 
-/**
- * @brief Key configuration structure
- */
-typedef struct {
-    tkey_event_cb_t event_cb;   /**< Event callback function */
-    tkey_detect_cb_t detect_cb; /**< Key detection callback function */
-    void *user_data; /**< User-defined data passed to both event and detection
-                        callbacks */
-    uint16_t hold_ticks;                 /**< Long press duration in ticks */
-    uint16_t debounce_ticks;             /**< Debounce time in ticks */
-    uint16_t multi_press_interval_ticks; /**< Multi-press interval in ticks */
-} tkey_config_t;
+struct tkey {
+    tkey_event_cb_t event_cb;
+    tkey_detect_cb_t detect_cb;
+    void *user_data;
+    volatile uint16_t hold_ticks;
+    volatile uint16_t debounce_ticks;
+    volatile uint16_t multi_press_interval_ticks;
+    volatile uint16_t pressed_ticks;
+    volatile uint16_t multi_press_ticks;
+    volatile uint8_t multi_press_count;
+    volatile uint8_t press_state;
+    volatile uint8_t flag_long_pressed;
+};
 
-/**
- * @brief Create a key instance with default configuration
- * @note Default key configration:
- * debounce_ticks:              10ms@100Hz
- * hold_ticks:                  500ms@100Hz
- * multi_press_interval_ticks:  300ms@100Hz
- *
- * @param event_cb Event callback function
- * @param detect_cb Detection callback function
- * @param user_data User-defined data passed to both event and detection
- * callbacks
- * @return
- * Key handle on success, NULL on failure
- */
-tkey_handle_t tkey_create_default(tkey_event_cb_t event_cb,
-                                  tkey_detect_cb_t detect_cb, void *user_data);
-
-/**
- * @brief Create a key instance with custom configuration
- *
- * @param config Key configuration parameters
- * @return
- * Key handle on success, NULL on failure
- */
-tkey_handle_t tkey_create(tkey_config_t *config);
-
-/**
- * @brief Delete a key instance and release its resources
- * @note This operation can only be performed when the key is disabled
- *
- * @param key Handle of the target key
- */
-void tkey_delete(tkey_handle_t key);
-
-/**
- * @brief Enable key processing
- * @note This function adds the key to the active key list
- *
- * @param key Handle of the target key
- */
-void tkey_enable(tkey_handle_t key);
-
-/**
- * @brief Disable key processing
- * @note This function removes the key from the active key list
- *
- * @param key Handle of the target key
- */
-void tkey_disable(tkey_handle_t key);
-
-/**
- * @brief Key event handler
- * @note This function should be called periodically at the configured frequency
- */
-void tkey_handler(void);
-
-/**
- * @brief Register or update callback functions for a key
- * @note Callbacks can only be registered when the key is not enabled
- *
- * @param key Handle of the target key
- * @param event_cb New event callback function
- * @param detect_cb New detection callback function
- * @param user_data User-defined data passed to both event and detection
- * callbacks
- */
-void tkey_register_callback(tkey_handle_t key, tkey_event_cb_t event_cb,
-                            tkey_detect_cb_t detect_cb, void *user_data);
-
-/**
- * @brief Set long press duration
- *
- * @param key Handle of the target key
- * @param hold_ticks Long press duration in system ticks
- */
-void tkey_set_hold(tkey_handle_t key, uint16_t hold_ticks);
-
-/**
- * @brief Set key debounce time
- *
- * @param key Handle of the target key
- * @param debounce_ticks Debounce time in system ticks
- */
-void tkey_set_debounce(tkey_handle_t key, uint16_t debounce_ticks);
-
-/**
- * @brief Set multi-press detection interval
- *
- * @param key Handle of the target key
- * @param multi_press_interval_ticks Multi-press interval in system ticks
- */
-void tkey_set_multi_press_interval(tkey_handle_t key,
-                                   uint16_t multi_press_interval_ticks);
-
-/**
- * @brief Get the current pressed duration
- *
- * @param key Handle of the target key
- * @return
- * Current pressed duration in ticks
- */
-uint16_t tkey_get_pressed_ticks(tkey_handle_t key);
-
-/**
- * @brief Get the current multi-press interval time
- *
- * @param key Handle of the target key
- * @return
- * Current multi-press interval time in ticks
- */
-uint16_t tkey_get_multi_press_ticks(tkey_handle_t key);
+int tkey_init(tkey_t *key, tkey_event_cb_t event_cb, tkey_detect_cb_t detect_cb,
+              void *user_data);
+int tkey_scan(tkey_t key_arr[], uint32_t num);
+void tkey_dispatch(uint8_t max_event_num);
+int tkey_set_hold(tkey_t *key, uint16_t hold_ticks);
+int tkey_set_debounce(tkey_t *key, uint16_t debounce_ticks);
+int tkey_set_multi_press_interval(tkey_t *key,
+                                  uint16_t multi_press_interval_ticks);
 
 #ifdef __cplusplus
 }
